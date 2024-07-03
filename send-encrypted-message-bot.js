fede26c705404414e9921c8c5fa4132986cb1b32
Krystian Bajno <krystian.bajno@gmail.com>
2024-07-03 23:38:53 +0200

    Update send-encrypted-message-bot.js

--- send-encrypted-message-bot.js
+++ send-encrypted-message-bot.js
@@ -9,15 +9,12 @@ const sendMessage = async (message) => {
   const base64EncodedKey = process.env.PUBLIC_KEY;
   const decodedKey = Buffer.from(base64EncodedKey, 'base64').toString('utf-8');
 
-  // Generate a random symmetric key
   const symmetricKey = crypto.randomBytes(32); // 256 bits for AES
 
-  // Encrypt the message with the symmetric key
   const iv = crypto.randomBytes(16); // Initialization vector for AES-256-CBC
   const cipher = crypto.createCipheriv('aes-256-cbc', symmetricKey, iv);
   const encryptedMessage = Buffer.concat([cipher.update(telegramMessage, 'utf-8'), cipher.final()]);
 
-  // Convert the symmetric key to a base64-encoded string
   const encryptedSymmetricKey = crypto.publicEncrypt(
     {
       key: decodedKey,
@@ -27,7 +24,6 @@ const sendMessage = async (message) => {
     symmetricKey
   ).toString('base64');
 
-  // Convert the encrypted message to base64-encoded strings
   const encryptedMessageBase64 = encryptedMessage.toString('base64');
   await bot.sendMessage(process.env.TELEGRAM_CHAT_ID, `${encryptedSymmetricKey};;${encryptedMessageBase64}`)
 };
